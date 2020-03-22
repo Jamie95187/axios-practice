ebeecb36c8d0dc15c0de2e0ce9c4518a71da6528
Jamie95187 <jamiewong63@googlemail.com>
2020-03-22 02:49:41 +0000

    style links

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,24 +1,37 @@
 import React, { Component } from 'react';
 import Posts from './Posts/Posts';
-import { Route, Link } from 'react-router-dom';
+import { Route, NavLink } from 'react-router-dom';
 import NewPost from './NewPost/NewPost';
 import './Blog.css';
 
 // Using Link means we only rerender where its needed oppose to an <a> tag.
+// Use NavLink to style links (and active links to style only links that are active)
 
 class Blog extends Component {
     render () {
+      console.log(this.props)
         return (
             <div>
               <header className="Blog">
                   <nav>
                     <ul>
-                      <li><Link to="/">Home</Link></li>
-                      <li><Link to={{
+                      <li><NavLink
+                        to="/"
+                        exact
+                        activeClassName="my-active"
+                        activeStyle={{
+                          color: '#fa923f',
+                          textDecoration: 'underline'
+                        }}>Home</NavLink></li>
+                      {/*can build dynamic path like this
+                        pathname: this.props.match.url + '/new-post'
+                        (appends path to current path)
+                        this is a relative path */}
+                      <li><NavLink to={{
                         pathname: '/new-post',
                         hash: '#submit',
                         search: '?quick-submit=true'
-                      }}>New Post</Link></li>
+                      }}>New Post</NavLink></li>
                     </ul>
                   </nav>
               </header>

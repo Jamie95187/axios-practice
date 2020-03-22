f51f245a15d37c35747e34fc2323cafe60450a6a
Jamie95187 <jamiewong63@googlemail.com>
2020-03-22 01:50:04 +0000

    change to only rerender where necessary

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import Posts from './Posts/Posts';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 import NewPost from './NewPost/NewPost';
 import './Blog.css';
+
+// Using Link means we only rerender where its needed oppose to an <a> tag. 
 
 class Blog extends Component {
     render () {
@@ -11,8 +13,12 @@ class Blog extends Component {
               <header className="Blog">
                   <nav>
                     <ul>
-                      <li><a href="/">Home</a></li>
-                      <li><a href="/new-post">New Post</a></li>
+                      <li><Link to="/">Home</Link></li>
+                      <li><Link to={{
+                        pathname: '/new-post',
+                        hash: '#submit',
+                        search: '?quick-submit=true'
+                      }}>New Post</Link></li>
                     </ul>
                   </nav>
               </header>
